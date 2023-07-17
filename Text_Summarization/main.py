7d86ac69a35d300b644fb066851192a06882403b
abhishek_singh <abhishek.singh@livnsense.com>
2023-07-17 12:27:04 +0530

    data ingestion notebook added

--- Text_Summarization/main.py
+++ Text_Summarization/main.py
@@ -1,3 +1,12 @@
+from textSummarizer.pipeline.stage_1_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.logging import logger
 
-logger.info('welcome !')
+STAGE_NAME  = 'Data Ingestion stage'
+try:
+    logger.info(f'>>>>>> stage {STAGE_NAME} started <<<<<<<')
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f'>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\n x===========x')
+except Exception as e:
+    logger.error(e)
+    raise e
---
+++ Text_Summarization/src/textSummarizer/constants/__init__.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path('Text_Summarization/config/config.yaml')
+PARAMS_FILE_PATH = Path('Text_Summarization/params.yaml')
