c26995bc8e1020b8951ec4ea734c448e7911fb3b
abhishek_singh <abhishek.singh@livnsense.com>
2023-07-22 19:35:24 +0530

    cpu training enabled

--- src/textSummarizer/conponents/model_trainer.py
+++ src/textSummarizer/conponents/model_trainer.py
@@ -13,7 +13,7 @@ class ModelTrainer:
 
     
     def train(self):
-        device = "cuda" if torch.cuda.is_available() else "mps"
+        device = "cuda" if torch.cuda.is_available() else "cpu"
         tokenizer = AutoTokenizer.from_pretrained(self.config.model_ckpt)
         model_pegasus = AutoModelForSeq2SeqLM.from_pretrained(self.config.model_ckpt).to(device)
         seq2seq_data_collator = DataCollatorForSeq2Seq(tokenizer, model=model_pegasus)
@@ -35,13 +35,12 @@ class ModelTrainer:
             per_device_train_batch_size=8, per_device_eval_batch_size=1,
             weight_decay=0.01, logging_steps=10,
             evaluation_strategy='steps', eval_steps=500, save_steps=1e6,
-            gradient_accumulation_steps=16,
-            use_mps_device=True
+            gradient_accumulation_steps=16
         ) 
 
         trainer = Trainer(model=model_pegasus, args=trainer_args,
                   tokenizer=tokenizer, data_collator=seq2seq_data_collator,
-                  train_dataset=dataset_samsum_pt["test"], 
+                  train_dataset=dataset_samsum_pt["train"], 
                   eval_dataset=dataset_samsum_pt["validation"])
         
         trainer.train()
